c37afd8491e8d26a3d4a3fe887ac0bbf30f99406
Rauf Endro Widagdo <raufendrowidagdo@MacBook-Air-Rauf.local>
2022-10-03 23:01:46 +0700

    update

--- lib/home/alarmobat.dart
+++ lib/home/alarmobat.dart
@@ -1,23 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_alarm_clock/flutter_alarm_clock.dart';
+import 'jamalarm/tiga.dart';
+import 'jamalarm/dua.dart';
+import 'jamalarm/namaobat.dart';
 
 class AlarmObat extends StatelessWidget {
   AlarmObat({Key? key}) : super(key: key);
   final myController = TextEditingController();
-
-  tigaKali() {
-    FlutterAlarmClock.createAlarm(05, 00);
-    Future.delayed(Duration(seconds: 3));
-    FlutterAlarmClock.createAlarm(13, 00);
-    Future.delayed(Duration(seconds: 3));
-    FlutterAlarmClock.createAlarm(21, 00);
-  }
-
-  duaKali() {
-    FlutterAlarmClock.createAlarm(07, 00);
-    Future.delayed(Duration(seconds: 3));
-    FlutterAlarmClock.createAlarm(19, 00);
-  }
+  final myController2 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -28,21 +18,101 @@ class AlarmObat extends StatelessWidget {
         body: Center(
             child: Column(
           children: [
-            Text("Minum obat berapa kali sehari?"),
-            TextField(
-              controller: myController,
-              keyboardType: TextInputType.number,
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Nama Obat",
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Stack(
+              children: [
+                SafeArea(
+                    child: Center(
+                  child: Container(
+                      padding: const EdgeInsets.all(5.0),
+                      child: TextField(
+                          textAlign: TextAlign.center,
+                          controller: myController2,
+                          decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Contoh : Paracetamol')),
+                      width: 283,
+                      height: 50,
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(25),
+                            topRight: Radius.circular(25),
+                            bottomLeft: Radius.circular(25),
+                            bottomRight: Radius.circular(25),
+                          ),
+                          color: Color.fromARGB(220, 220, 220, 220))),
+                )),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Minum obat berapa kali sehari?",
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Stack(
+              children: [
+                SafeArea(
+                    child: Center(
+                  child: Container(
+                      padding: const EdgeInsets.all(5.0),
+                      child: TextField(
+                          textAlign: TextAlign.center,
+                          controller: myController,
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Contoh : 3')),
+                      width: 283,
+                      height: 50,
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(25),
+                            topRight: Radius.circular(25),
+                            bottomLeft: Radius.circular(25),
+                            bottomRight: Radius.circular(25),
+                          ),
+                          color: Color.fromARGB(220, 220, 220, 220))),
+                )),
+              ],
             ),
             ElevatedButton(
                 onPressed: () {
+                  namaobat = myController2.text;
                   if (myController.text == '3') {
-                    tigaKali();
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation1, animation2) =>
+                            Tiga(),
+                        transitionDuration: Duration(seconds: 0),
+                      ),
+                    );
                   }
                   if (myController.text == '2') {
-                    duaKali();
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation1, animation2) => Dua(),
+                        transitionDuration: Duration(seconds: 0),
+                      ),
+                    );
                   }
                 },
-                child: Text('Simpan'))
+                child: Text('Lanjut'))
           ],
         )));
   }
---
+++ lib/home/jamalarm/tiga.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_alarm_clock/flutter_alarm_clock.dart';
+import 'namaobat.dart';
+
+class Tiga extends StatelessWidget {
+  Tiga({Key? key}) : super(key: key);
+  final myController1 = TextEditingController();
+  final myController2 = TextEditingController();
+  final myController3 = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Health News ID'),
+        ),
+        body: SafeArea(
+          child: Center(
+              child: Column(
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Alarm Pertama",
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Stack(
+                children: [
+                  SafeArea(
+                      child: Center(
+                    child: Container(
+                        padding: const EdgeInsets.all(5.0),
+                        child: TextField(
+                            textAlign: TextAlign.center,
+                            controller: myController1,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Contoh : 05.00')),
+                        width: 283,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(25),
+                              topRight: Radius.circular(25),
+                              bottomLeft: Radius.circular(25),
+                              bottomRight: Radius.circular(25),
+                            ),
+                            color: Color.fromARGB(220, 220, 220, 220))),
+                  )),
+                ],
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    final jam = myController1.text.split('.');
+                    var h = jam[0];
+                    var m = jam[1];
+                    FlutterAlarmClock.createAlarm(int.parse(h), int.parse(m),
+                        title: 'Alarm Obat Pertama (' + namaobat + ')');
+                  },
+                  child: Text('Simpan')),
+              SizedBox(
+                height: 25,
+              ),
+              Text(
+                "Alarm Kedua",
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Stack(
+                children: [
+                  SafeArea(
+                      child: Center(
+                    child: Container(
+                        padding: const EdgeInsets.all(5.0),
+                        child: TextField(
+                            textAlign: TextAlign.center,
+                            controller: myController2,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Contoh : 05.00')),
+                        width: 283,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(25),
+                              topRight: Radius.circular(25),
+                              bottomLeft: Radius.circular(25),
+                              bottomRight: Radius.circular(25),
+                            ),
+                            color: Color.fromARGB(220, 220, 220, 220))),
+                  )),
+                ],
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    final jam = myController2.text.split('.');
+                    var h = jam[0];
+                    var m = jam[1];
+                    FlutterAlarmClock.createAlarm(int.parse(h), int.parse(m),
+                        title: 'Alarm Obat Kedua (' + namaobat + ')');
+                  },
+                  child: Text('Simpan')),
+              SizedBox(
+                height: 25,
+              ),
+              Text(
+                "Alarm Ketiga",
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Stack(
+                children: [
+                  SafeArea(
+                      child: Center(
+                    child: Container(
+                        padding: const EdgeInsets.all(5.0),
+                        child: TextField(
+                            textAlign: TextAlign.center,
+                            controller: myController3,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Contoh : 05.00')),
+                        width: 283,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(25),
+                              topRight: Radius.circular(25),
+                              bottomLeft: Radius.circular(25),
+                              bottomRight: Radius.circular(25),
+                            ),
+                            color: Color.fromARGB(220, 220, 220, 220))),
+                  )),
+                ],
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    final jam = myController3.text.split('.');
+                    var h = jam[0];
+                    var m = jam[1];
+                    FlutterAlarmClock.createAlarm(int.parse(h), int.parse(m),
+                        title: 'Alarm Obat Ketiga (' + namaobat + ')');
+                  },
+                  child: Text('Simpan'))
+            ],
+          )),
+        ));
+  }
+}
